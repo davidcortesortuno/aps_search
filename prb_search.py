cb83d9ca29ed66acf9f3be0e9dfc7f0b19b60b1b
davidcorteso <d.i.cortes@soton.ac.uk>
2016-10-21 13:01:09 +0100

    Filtered results using JSON, added keyword argument using argparse and added  printed results. We can organise this in a class later

--- prb_search.py
+++ prb_search.py
@@ -1,11 +1,28 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import json
+import argparse
 
+parser = argparse.ArgumentParser(description='PRB Journal Search')
 
-response = urllib.request.urlopen('http://journals.aps.org/search/results?page=1&date=&sort=recent&per_page=20&start_date=&end_date=&clauses=%5B%7B%22operator%22:%22AND%22,%22field%22:%22all%22,%22value%22:%22skyrmion%22%7D%5D#title')
+parser.add_argument('-kw', '--keyword', help='Search keyword',
+                    default='skyrmion')
+
+# Parser arguments
+args = parser.parse_args()
+
+# -----------------------------------------------------------------------------
+
+url = ('http://journals.aps.org/search/'
+       'results?page=1&date=&sort=recent&per_page=20'
+       '&start_date=&end_date=&clauses=%5B%7B%22operator%22:'
+       '%22AND%22,%22field%22:%22all%22,%22value%22:'
+       '%22{0}%22%7D%5D#title').format(args.keyword)
+
+response = urllib.request.urlopen(url)
 html = response.read()
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "lxml")
 
 # Search for contents in the tags <script>...</script>
 # From the APS results, the latest <script> is the one we need, which
@@ -16,5 +33,18 @@ results = soup.find_all('script')[-1]
 # an "action:" string, thus we separate the whole text by this pattern
 # The 0th element is irrelevant, it contains the window.results string
 results = results.text.split(r'{"actions":')[1:]
+results[-1] = results[-1].split('"facets"')[0][:-2]
+results = [r'{"actions":' + r.rstrip(',') for r in results]
 
-# Now we can regex the patterns
+results = [json.loads(r) for r in results]
+
+for r in results:
+    print('{:<11} {:<}'.format(r['date'], r['title']))
+    print('{:<11} {:<}'.format('', r['authors']))
+    print('{:<11} {:<}'.format('', r['journal']))
+    print('{:<11} {:<}'.format('',
+                               'http://http://journals.aps.org/' +
+                               r['actions']['pdf']['link']
+                               )
+          )
+    print('-' * 80)
