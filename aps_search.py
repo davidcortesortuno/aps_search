d9a2ec444dc3520816e9ae69371a4c06ac8b0ba4
davidcorteso <dic1e13@soton.ac.uk>
2017-01-25 22:51:35 +0000

    Wrapped outputs to 80 lines. Added bold style to titles

--- aps_search.py
+++ aps_search.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import json
 import argparse
 import re
+import textwrap as tw
+
+NWRAP = 68
 
 parser = argparse.ArgumentParser(description='PRB Journal Search')
 
@@ -22,6 +25,7 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+    BOLD = '\033[1m'
 
 # -----------------------------------------------------------------------------
 
@@ -52,13 +56,17 @@ results = [json.loads(r) for r in results]
 
 for r in results:
     r['title'] = re.sub(r'<[^>]*>', '', r['title'])
+    r['title'] = bcolors.BOLD + r['title'] + bcolors.ENDC
+    r['title'] = tw.fill(r['title'], NWRAP).replace('\n', '\n' + ' ' * 13)
+
+    r['authors'] = tw.fill(r['authors'], NWRAP).replace('\n', '\n' + ' ' * 13)
 
 for r in results:
-    print('{:<11} {:<}'.format(bcolors.OKBLUE + r['date'] + bcolors.ENDC,
+    print('{:<21} {:<}'.format(bcolors.OKBLUE + r['date'] + bcolors.ENDC,
                                r['title']))
-    print('{:<11} {:<}'.format('', r['authors']))
-    print('{:<11} {:<}'.format('', bcolors.OKGREEN + r['journal'] + bcolors.ENDC))
-    print('{:<11} {:<}'.format('',
+    print('{:<12} {:<}'.format('', r['authors']))
+    print('{:<12} {:<}'.format('', bcolors.OKGREEN + r['journal'] + bcolors.ENDC))
+    print('{:<12} {:<}'.format('',
                                'http://journals.aps.org/' +
                                r['actions']['pdf']['link']
                                )
